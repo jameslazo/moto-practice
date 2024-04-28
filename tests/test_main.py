85167dfe8e0b65facc5b10a7b5c2209e8dd60166
jameslazo <james.lazo@gmail.com>
2024-04-27 17:39:01 -0700

    "first test"

---
+++ tests/test_main.py
@@ -0,0 +1,39 @@
+import unittest
+from decimal import Decimal
+import boto3
+from moto import mock_aws
+from src import main
+
+
+
+class DDBMotoTest(unittest.TestCase):
+
+    @mock_aws
+    def test_table_put_item(self):
+        main.dynamo_resource = None
+        boto3.setup_default_session()
+        client = boto3.client("dynamodb", region_name="us-east-1")
+        client.create_table(
+            TableName="test-table",
+            KeySchema=[
+                {"AttributeName": "partitionKey", "KeyType": "HASH"}
+            ],
+            AttributeDefinitions=[
+                {"AttributeName": "partitionKey", "AttributeType": "S"}
+            ]
+        )
+
+        response = main.table_put_item()
+        self.assertEqual({'CapacityUnits': 1, 'TableName': 'test-table'}, response["ConsumedCapacity"])
+        self.assertEqual(200, response["ResponseMetadata"]["HTTPStatusCode"])
+        self.assertEqual(
+            {
+            'partitionKey': {'S': 'id'},
+            'name': {'S': 'testname'},
+            'value': {'N': '1'}
+            },
+            client.get_item(
+                TableName="test-table",
+                Key={"partitionKey": {"S": "id"}}
+                )["Item"]
+        )

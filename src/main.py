cccc295e41acdc30fe67d8c7361d4719731fcda6
jameslazo <james.lazo@gmail.com>
2024-04-27 18:06:50 -0700

    main.py update

--- src/main.py
+++ src/main.py
@@ -9,12 +9,28 @@ def init_dynamodb():
     if dynamo_resource is None:
         dynamo_resource = boto3.resource("dynamodb", region_name="us-east-1")
 
-def table_put_item():
+def ddb_operations(event: any):
     init_dynamodb()
-    record = {
-        "partitionKey": "id",
-        "name": "testname",
-        "value": 1
-    }
-    table = dynamo_resource.Table("test-table")
-    return table.put_item(Item=record)
+    
+    if event == "put_item":
+        record = {
+            "partitionKey": "id",
+            "name": "testname",
+            "value": 1
+        }
+        table = dynamo_resource.Table("test-table")
+        return table.put_item(Item=record)
+    elif event == "scan":
+        done = False
+        start_key = None
+        items = []
+        while not done:
+            if start_key:
+                response = table.scan(FilterExpression=Attr("name").eq("placeholder"), ExclusiveStartKey=start_key)
+            else:
+                response = table.scan(FilterExpression=Attr("name").eq("placeholder"))
+            for item in response.get("Items", []):
+                items.append(item)
+            start_key = response.get('LastEvaluatedKey', None)
+            done = start_key is None
+        return items
